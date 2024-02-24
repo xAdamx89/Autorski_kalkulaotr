895f8ab11d06b7b22b95ed179b41734fb0934099
Adam <159622934+xAdamx89@users.noreply.github.com>
2024-02-24 16:31:43 +0100

    V 1.0
    Dodane możliwości kalkulatora:
    - obliczanie wyznacznika macierzy 3x3

---
+++ Kalkulator/Kalkulator na algebre z geometrią/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Kalkulator_na_algebre_z_geometrią
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            Wyznacznik = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F);
+            label1.Location = new Point(148, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(514, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Kalkulator obliczający wyznacznik macierzy 3x3";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(65, 155);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(65, 197);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(65, 237);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(186, 155);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(186, 197);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(186, 237);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(303, 155);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 7;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(303, 197);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 8;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(303, 237);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(100, 23);
+            textBox9.TabIndex = 9;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(148, 293);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 10;
+            button1.Text = "Oblicz";
+            button1.UseVisualStyleBackColor = true;
+            button1.MouseClick += button1_MouseClick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(501, 155);
+            label2.Name = "label2";
+            label2.Size = new Size(191, 15);
+            label2.TabIndex = 11;
+            label2.Text = "Wyznacznik danej macierzy wynosi";
+            // 
+            // Wyznacznik
+            // 
+            Wyznacznik.AutoSize = true;
+            Wyznacznik.Font = new Font("Segoe UI", 15F);
+            Wyznacznik.Location = new Point(567, 192);
+            Wyznacznik.Name = "Wyznacznik";
+            Wyznacznik.Size = new Size(0, 28);
+            Wyznacznik.TabIndex = 12;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(261, 293);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 13;
+            button2.Text = "Reset";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(Wyznacznik);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Kalkulator algebra z geometrią";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private Button button1;
+        private Label label2;
+        private Label Wyznacznik;
+        private Button button2;
+    }
+}
